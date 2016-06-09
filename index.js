03b4ffc5f773188dd5151280df33299be20ea2e5
evwilkin <evan.wilkinson@gmail.com>
2016-06-09 15:00:52 -0700

    final code for one route

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 //Dependencies
 var express = require("express");
 var app = express();
+var mongoose = require("mongoose");
+var bodyParser = require('body-parser');
+
+//MongoDB
+mongoose.connect('mongodb://localhost:27017/node_restful_test');
+
+//Express
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 //Routes
-app.get('/', function(req, res) {
-  res.send("Oh hi there Evan");
-});
+app.use('/api', require('./routes/api'));
 
+//Start Server
 app.listen(3000);
 console.log("API running on port 3000");
